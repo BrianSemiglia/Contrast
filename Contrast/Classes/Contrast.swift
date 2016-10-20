e5ab586df45fd3eb2f3cf8d5e32fac7b117cf4d5
Brian Semiglia <brian.semiglia@gmail.com>
2016-10-20 13:40:42 -0400

    Adds Contrast and tests.

---
+++ Contrast/Classes/Contrast.swift
@@ -0,0 +1,169 @@
+//
+//  Contrast.swift
+//  Contrast
+//
+//  Created by Brian Semiglia on 10/15/16.
+//  Copyright © 2016 Brian Semiglia. All rights reserved.
+//
+
+import Foundation
+
+public struct Shift<T: Equatable> {
+  let element: T
+  let origin: Int
+  let destination: Int
+  
+  public init(element: T, origin: Int, destination: Int) {
+    self.element = element
+    self.origin = origin
+    self.destination = destination
+  }
+}
+
+extension Shift: Equatable {
+  public static func ==(left: Shift, right: Shift) -> Bool { return
+    left.origin == right.origin &&
+    left.destination == right.destination &&
+    left.element == right.element
+  }
+}
+
+public struct PathShift<T> {
+  let element: T
+  let origin: IndexPath
+  let destination: IndexPath
+  
+  public init(element: T, origin: IndexPath, destination: IndexPath) {
+    self.element = element
+    self.origin = origin
+    self.destination = destination
+  }
+}
+
+extension PathShift: Equatable {
+  public static func ==(left: PathShift, right: PathShift) -> Bool { return
+    left.origin == right.origin &&
+    left.destination == right.destination
+  }
+}
+
+public struct Indexed<T: Equatable> {
+  let element: T
+  let index: Int
+  public init(element: T, index: Int) {
+    self.element = element
+    self.index = index
+  }
+}
+
+extension Indexed: Equatable {
+  public static func ==(left: Indexed, right: Indexed) -> Bool { return
+    left.element == right.element &&
+    left.index == right.index
+  }
+}
+
+public struct PathIndexed<T: Equatable> {
+  let element: T
+  let index: IndexPath
+  public init(element: T, index: IndexPath) {
+    self.element = element
+    self.index = index
+  }
+}
+
+extension PathIndexed: Equatable {
+  public static func ==(left: PathIndexed, right: PathIndexed) -> Bool { return
+    left.index == right.index &&
+    left.element == right.element
+  }
+}
+
+public extension Collection where Iterator.Element: Hashable {
+
+  public func shifts(_ input: Self) -> [Shift<Iterator.Element>] { return
+    enumerated().flatMap { item in return
+      input.enumerated()
+      .filter { $0.element == item.element && $0.offset != item.offset }
+      .map { match in
+        Shift(
+          element: item.element,
+          origin: item.offset,
+          destination: match.offset
+        )
+      }
+    }
+  }
+  
+  public func additions(_ input: Self) -> [Indexed<Iterator.Element>] { return
+    input
+    .enumerated()
+    .filter { !contains($0.element) }
+    .map { Indexed(element: $0.element, index: $0.offset) }
+  }
+  
+  public func deletions(_ input: Self) -> [Indexed<Iterator.Element>] { return
+    enumerated()
+    .filter { !input.contains($0.element) }
+    .map { Indexed(element: $0.element, index: $0.offset) }
+  }
+  
+}
+
+public extension Array where Element: Collection, Element.Iterator.Element: Hashable {
+  
+  public func shifts(_ input: [Element]) -> [PathShift<Element.Iterator.Element>] { return
+    indexedFlattened().flatMap { item in return
+      input.indexedFlattened()
+      .filter { $0.element == item.element && $0.index != item.index }
+      .map { match in
+        PathShift(
+          element: item.element,
+          origin: item.index,
+          destination: match.index
+        )
+      }
+    }
+  }
+  
+  public func additions(_ input: [Element]) -> [PathIndexed<Element.Iterator.Element>] { return
+    input.indexedFlattened().flatMap { item in
+      if !indexedFlattened().contains(where: { $0.element == item.element }) { return
+        PathIndexed(
+          element: item.element,
+          index: item.index
+        )
+      } else { return
+        nil
+      }
+    }
+  }
+  
+  public func deletions(_ input: [Element]) -> [PathIndexed<Element.Iterator.Element>] { return
+    indexedFlattened().flatMap { item in
+      if !input.indexedFlattened().contains(where: { $0.element == item.element }) { return
+        PathIndexed(
+          element: item.element,
+          index: item.index
+        )
+      } else { return
+        nil
+      }
+    }
+  }
+  
+  public func indexedFlattened() -> [PathIndexed<Element.Iterator.Element>] { return
+    enumerated().flatMap { section in
+      section.element.enumerated().map { item in
+        PathIndexed(
+          element: item.element,
+          index: IndexPath(
+            row: item.offset,
+            section: section.offset
+          )
+        )
+      }
+    }
+  }
+
+}
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -3,27 +3,127 @@ import XCTest
 import Contrast
 
 class Tests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
+
+  func testShiftSome() {
+    XCTAssert(
+      ["a", "b"].shifts(["b", "a"])
+      ==
+      [
+        Shift(element: "a", origin: 0, destination: 1),
+        Shift(element: "b", origin: 1, destination: 0)
+      ]
+    )
+  }
+  
+  func testShiftAdditions() {
+    XCTAssert(
+      ["a", "b"].shifts(["b", "a", "c"])
+      ==
+      [
+        Shift(element: "a", origin: 0, destination: 1),
+        Shift(element: "b", origin: 1, destination: 0)
+      ]
+    )
+  }
+  
+  func testShiftNoneReplaceAll() {
+    XCTAssert(
+      ["a", "b", "c"].additions(["d", "e", "f"])
+      ==
+      [
+        Indexed(element: "d", index: 0),
+        Indexed(element: "e", index: 1),
+        Indexed(element: "f", index: 2)
+      ]
+    )
+  }
+  
+  func testIntersectionalRowShift() {
+    XCTAssert(
+      [["a", "b"], ["c", "d"]].shifts([["a", "d"], ["c", "b"]])
+      ==
+      [
+        PathShift(
+          element: "b",
+          origin: IndexPath(row: 1, section: 0),
+          destination: IndexPath(row: 1, section: 1)
+        ),
+        PathShift(
+          element: "d",
+          origin: IndexPath(row: 1, section: 1),
+          destination: IndexPath(row: 1, section: 0)
+        )
+      ]
+    )
+  }
+  
+  func testMoveNotReturnedAsDeletion() {
+    XCTAssert(
+      [["a", "b"], ["c", "d"]].deletions([["a", "d"], ["c", "b"]])
+      ==
+      [PathIndexed<String>]()
+    )
+  }
+  
+  func testDeletions() {
+    XCTAssert(
+      [["a", "b"], ["c", "d"]].deletions([["a", "d"], ["b"]])
+      ==
+      [
+        PathIndexed(
+          element: "c",
+          index: IndexPath(
+            row: 0,
+            section: 1
+          )
+        )
+      ]
+    )
+  }
+  
+  func testDeletionsNone() {
+    XCTAssert(
+      [["a", "b"], ["c", "d"]].deletions([["a", "d"], ["c", "b"]])
+      ==
+      [PathIndexed<String>]()
+    )
+  }
+  
+  func testFlattening() {
+    XCTAssert(
+      [["a", "b"], ["c", "d"]].indexedFlattened()
+      ==
+      [
+        PathIndexed(element: "a", index: IndexPath(row: 0, section: 0)),
+        PathIndexed(element: "b", index: IndexPath(row: 1, section: 0)),
+        PathIndexed(element: "c", index: IndexPath(row: 0, section: 1)),
+        PathIndexed(element: "d", index: IndexPath(row: 1, section: 1))
+      ]
+    )
+  }
+  
+  func testAdditions() {
+    XCTAssert(
+      [["a", "b"], ["c", "d"]].additions([["a", "d"], ["c", "b", "e"]])
+      ==
+      [
+        PathIndexed(
+          element: "e",
+          index: IndexPath(
+            row: 2,
+            section: 1
+          )
+        )
+      ]
+    )
+  }
+  
+  func testAdditionsNone() {
+    XCTAssert(
+      [["a", "b"], ["c", "d"]].additions([["a", "d"], ["c", "b"]])
+      ==
+      [PathIndexed<String>]()
+    )
+  }
     
 }
